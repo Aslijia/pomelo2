cabc57ecf17bf056a75b62dd51535b4590c2ef9f
Aslijia <medivh.wd@gamail.com>
2019-12-30 11:35:23 +0800

    调整日志

---
+++ src/components/connector.ts
@@ -0,0 +1,25 @@
+
+
+export class ConnectorComponent {
+    name: string = '__connector__';
+
+    async start() {
+
+    }
+
+    async afterStart() {
+
+    }
+
+    async stop() {
+
+    }
+
+    async send() {
+
+    }
+
+    async sendAsync() {
+
+    }
+}
